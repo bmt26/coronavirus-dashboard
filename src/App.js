bc2b03e853481fe14f9fcd1f61e9202d75ec9f69
John Houlihan <ec2-user@ip-172-31-68-179.ec2.internal>
2021-04-13 17:45:29 +0000

    Added support for Google OAuth Login/Logout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { React, useState, useEffect } from 'react';
+import Login from './Login';
+import Logout from './Logout';
+
 import './App.css';
 
 function App() {
+  
+  const [loggedIn, setLoggedIn] = useState(false);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login />
+      <Logout />
     </div>
   );
 }
