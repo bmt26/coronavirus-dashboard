a8479d0e20f28d210adf1690733d35e98217bf75
Jhon Garcia <ec2-user@ip-172-31-10-40.ec2.internal>
2021-04-16 20:20:10 +0000

    Added a simple dropdown list.

--- src/SetCountry.js
+++ src/SetCountry.js
@@ -1,29 +1,20 @@
 import React from 'react';
 
 function SetCountry() {
-    
-    var myParent = document.body;
-
-    //Create array of options to be added
-    var array = ["Volvo","Saab","Mercades","Audi"];
-    
-    //Create and append select list
-    var selectList = document.createElement("select");
-    selectList.id = "mySelect";
-    myParent.appendChild(selectList);
-    
-    //Create and append the options
-    for (var i = 0; i < array.length; i++) {
-        var option = document.createElement("option");
-        option.value = array[i];
-        option.text = array[i];
-        selectList.appendChild(option);
-    }
-    
     return(
-        <select id="selectNumber">
-            <option>Choose a number</option>
-        </select>
+        <div>
+            <form action="/action_page.php">
+              <label for="cars">Choose a car:</label>
+              <select name="cars" id="cars">
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+              <br></br>
+              <input type="submit" value="Submit"/>
+            </form>
+        </div>
     );
 }
 
