de1f67123bc5530bca6d8df7b67a7cc592499f6c
John Houlihan <ec2-user@ip-172-31-68-179.ec2.internal>
2021-04-16 03:43:34 +0000

    Changed User database to UserData database - User is a reserved table

--- models.py
+++ models.py
@@ -9,12 +9,12 @@ The User database is defined with the following parameters:
 
 from app import DB
 
-class User(DB.Model):
+class UserData(DB.Model):
     """ Declare the User database with the necessary parameters """
     email = DB.Column(DB.String(100), primary_key=True)
-    name = DB.Coloumn(DB.String(80), unique=False, nullable=False)
-    image = DB.Coloumn(DB.String(120), unique=True, nullable=False)
-    country = DB.Coloumn(DB.String(50), unique=False, nullable=True)
+    name = DB.Column(DB.String(80), unique=False, nullable=False)
+    image = DB.Column(DB.String(120), unique=True, nullable=False)
+    country = DB.Column(DB.String(50), unique=False, nullable=True)
 
     def __repr__(self):
         """ Return the User's name """
