30a452a5f26ecce6c8eccf976b84d7c9b2def91f
Jhon Garcia <ec2-user@ip-172-31-10-40.ec2.internal>
2021-04-27 23:36:39 +0000

    Created UsersTable file to start working on it.

---
+++ src/UsersTable.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './setcountry.css';
+import io from 'socket.io-client';
+
+const socket = io();
+
+function UsersTable() {
+
+  return (
+      <div>
+      </div>
+  );
+}
+
+export default UsersTable;
