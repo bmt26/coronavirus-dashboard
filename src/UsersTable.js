4c7da39b05f1c4ba02d9fefbf975f58e3d5698a8
Jhon Garcia <ec2-user@ip-172-31-10-40.ec2.internal>
2021-04-28 23:41:26 +0000

    Working on users table.

--- src/UsersTable.js
+++ src/UsersTable.js
@@ -4,8 +4,19 @@ import io from 'socket.io-client';
 
 const socket = io();
 
+// Call <UsersTable /> in the Authentication.js file under
+// <SetCountry />
+
 function UsersTable() {
 
+  // Leaderboard state
+  const [users, setUsers] = useState([]);
+  const [homeCountries, setHomeCountries] = useState([]);
+  
+  // Get users and home countries list then display them
+
+  
+  
   return (
       <div>
       </div>
