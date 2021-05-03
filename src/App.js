5e8087d170b597033736a41346f3fdd2794f1fb7
John Houlihan <ec2-user@ip-172-31-68-179.ec2.internal>
2021-05-03 17:56:52 +0000

    Added support for client-side search bar for user input

--- src/App.js
+++ src/App.js
@@ -11,11 +11,28 @@ const socket = io();
 
 // Main driver function
 function App() {
+  // Reference to SearchCountry component
+  const [country, setCountry] = useState('');
+
+  // Declare Area and setArea variables
   const [Area, setArea] = useState([]);
+
+  // Function to handle searching for a desired country
+  function onClickSearch() {
+    // Emit the 'search_country' event to the server
+    console.log({ country })
+    socket.emit('search_country', { country });
+  }
+
   return (
     <div className="App">
       <Authentication socket={socket} />
-      <SearchCountry />
+      <SearchCountry
+        onChange={(event) => setCountry(event.target.value)}
+        value={country}
+        disabled={country.length === 0}
+        onClick={onClickSearch}
+      />
       <Table Area={Area} setArea={setArea} />
     </div>
   );
--- src/SearchCountry.js
+++ src/SearchCountry.js
@@ -2,10 +2,25 @@ import { React, useState } from 'react';
 
 // Function to handle user Search
 function SearchCountry(props) {
+    const {
+        onChange, country, disabled, onClick,
+    } = props;
+    // Display the search bar
     return (
         <div>
-            <input type='text' class='searchCountryInput' id='searchCountryInput' name='searchCountryInput' />
-            <button class='searchCountryButton' id='searchCountryButton'> Search </button>
+            <input
+                required
+                id="searchCountryInput"
+                onChange={onChange}
+                aria-label="search-input"
+                value={country}
+                type="text"
+            />
+            <button
+                className="searchCountryButton"
+                type="submit"
+                disabled={disabled}
+                onClick={onClick}> Search </button>
         </div>
     );
 }
