77a68c96d32f90aa202d934d5f6b8fbaac244a38
Brian Temple <bmt26@njit.edu>
2021-05-04 18:51:56 +0000

    Text format error fixed

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -5,18 +5,18 @@ function LandingPage() {
       <div class="text">
         <h1>About</h1>
         <div class="para">
-          <p>
+          <p class="land">
             &emsp;&emsp;We designed this website with the purpose to provide statistics on the
             COVID-19. Due to this unprecedented time is extremely important to know how countries
             and states are doing in regards to the number of deaths, confirmed cases, total deaths,
             and the people that have recovered from it.
           </p>
-          <p>
+          <p class="land">
             We also wanted people to be able to register on our website and add a home country if
             desired as well as compare countries by their statistics.
           </p>
-          <p>This web app was built using the Google and Covid-19 API's.</p>
-          <p>
+          <p class="land">This web app was built using the Google and Covid-19 API's.</p>
+          <p class="land">
             This web app was built by the combined efforts of Brian Temple, Jhon Garcia, John
             Houlihan, and Shail Parekh.
           </p>
